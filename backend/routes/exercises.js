aa134f25429890e721eb1e7aec527135d304fb1c
Roger Winright <rogerwinright@gmail.com>
2019-11-25 18:42:56 -0500

    added delete route

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,13 +1,28 @@
 const router = require('express').Router();
 let Exercise = require("../models/exercise.model");
 
-router.route('/').get((req, res)=>{ //First endpoint that handles incoming HTTP get requests
+router.route('/').get((req, res) => { //First endpoint that handles incoming HTTP get requests
   Exercise.find() //Get the users collection
-  .then(users => res.json(users)) //Return the response as json
-  .then(err => res.status(400).json('Error', err)); //If it's not found, error out.
+    .then(users => res.json(users)) //Return the response as json
+    .then(err => res.status(400).json('Error', err)); //If it's not found, error out.
 });
 
-router.route('/add').post((req, res)=> {
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username,
+        exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error', err))
+    })
+    .catch(err => res.status(400).json('Error', err))
+});
+
+router.route('/add').post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
@@ -21,7 +36,7 @@ router.route('/add').post((req, res)=> {
   })
 
   exercise.save() //Saves the information to the DB
-    .then(()=> res.json('User added!'))
+    .then(() => res.json('Exercise added!'))
     .catch(err => res.status(400).json('Error', err))
 });
 
