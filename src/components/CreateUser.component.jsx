d7bd8f41c2d63f29076196b75630883bebe12009
Roger Winright <rogerwinright@gmail.com>
2019-11-30 00:53:41 -0500

    added user component

--- src/components/CreateUser.component.jsx
+++ src/components/CreateUser.component.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CreateUser() {
+
+  const [userName, setUserName] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: userName
+    }
+    console.log(data);
+    setUserName("")
+  }
+  
   return (
     <div>
-       <h1>Create Users Component</h1>
+      <h3>Create New User</h3>
+
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input
+            type="text"
+            className="form-control"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">Submit User</button>
+        </div>
+      </form>
     </div>
   )
 }
